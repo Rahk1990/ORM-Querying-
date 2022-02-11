46f006ffdf6c25a8dab1bbdcd86062cf1758e532
h-latta <h.latta@yahoo.com>
2022-02-11 14:30:17 -0600

    Completed Problem 4

--- school_db/views.py
+++ school_db/views.py
@@ -189,7 +189,14 @@ SELECT `school_db_instructor`.`id`,
 
 # Get the count of students, courses, and instructors and print them in the terminal
 def problem_four(request):
+    student_count = Student.objects.count()
+    print(f'Student Count: {student_count}')
 
+    course_count = Course.objects.count()
+    print(f'Course Count: {course_count}')
+
+    instructor_count = Instructor.objects.count()
+    print(f'Instructor Count: {instructor_count}')
 
 
     return complete(request)
