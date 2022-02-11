e927c97d9e50377dc7187ab6b9e3b54d6e561d31
h-latta <h.latta@yahoo.com>
2022-02-11 15:17:08 -0600

    Completed Problem 7

--- school_db/views.py
+++ school_db/views.py
@@ -335,6 +335,8 @@ def problem_seven(request):
     # Make sure to set this equal to the primary key of the row you just created!
     student_id = 11
 
+    remove_student = Student.objects.get(id=11)
+    Student.objects.filter(id=11).delete()
 
     try:
         student = Student.objects.get(pk=student_id)
